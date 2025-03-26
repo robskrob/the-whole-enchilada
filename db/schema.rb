88c0ebedd0f550c203eced503193e35cc033c102
=robskrob <agentcooper_aces@proton.me>
2025-03-26 13:34:21 -0400

    add recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_24_141107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "title", limit: 150, null: false
+    t.text "description"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
